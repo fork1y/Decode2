1293d192b08164be4b1a631d7539fafc1f8dc885
Yen Nguyen <yenguyen190@gmail.com>
2026-03-22 17:49:15 -0700

    Create signExt.v

---
+++ signExt.v
@@ -0,0 +1,11 @@
+module signExtend(
+  input wire [15:0] immediate,
+  output wire [31:0] extended
+);
+  //pre-pend immediate with 16 bits of the MSB
+  //{} - concatenate or replicate operator 
+  //concatenate {111,100} -> 111000
+  //replicate {4{1}} -> 1111
+  assign extended = /*COMPLETE*/
+
+    endmodule
